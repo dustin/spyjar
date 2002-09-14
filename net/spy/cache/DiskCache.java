e8b9e32eeacf621db9eab78a59df1cc0d221f409
Dustin Sallings <dustin@spy.net>
2002-09-14 01:27:48 +0000

    DiskCache now fully implements the Map interface. Keywords: cscvs:MAIN-36
    Author: dustin
    Date: 2002/09/13 17:27:48
    
    imported from: dustin@spy.net--java/spyjar--sfcvs--1.0--patch-36

--- net/spy/cache/DiskCache.java
+++ net/spy/cache/DiskCache.java
@@ -1,5 +1,5 @@
 // Copyright (c) 1999 Dustin Sallings <dustin@spy.net>
-// $Id: DiskCache.java,v 1.3 2002/09/13 06:52:23 dustin Exp $
+// $Id: DiskCache.java,v 1.4 2002/09/13 17:27:49 dustin Exp $
 
 package net.spy.cache;
 
@@ -15,11 +15,11 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.HashSet;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.AbstractMap;
-import java.util.AbstractSet;
 
 import net.spy.util.NestedRuntimeException;
 
@@ -29,19 +29,10 @@ import net.spy.util.NestedRuntimeException;
  * This is used for terribly simple caches with no expiration dates on
  * objects.  Things go in and they stay in.
  */
-public class DiskCache extends Object {
-
-	private static final String DEFAULT_DIR="/tmp/diskcache";
+public class DiskCache extends AbstractMap {
 
 	// Base directory for hashing
 	private String basedir = null;
-
-	/**
-	 * Get a DiskCache object using the default directory.
-	 */
-	public DiskCache() {
-		this(DEFAULT_DIR);
-	}
 
 	/**
 	 * Get an DiskObject using the given directory.
@@ -131,12 +122,12 @@ public class DiskCache extends Object {
 		try {
 			FileInputStream istream = new FileInputStream(getPath(key));
 			ObjectInputStream p = new ObjectInputStream(istream);
-			String storedName = (String)p.readObject();
+			Object storedKey = p.readObject();
 			Object o = p.readObject();
 
-			if(!key.equals(storedName)) {
+			if(!key.equals(storedKey)) {
 				throw new Exception("Key value did not match ("
-					+ storedName + " != " + key + ")");
+					+ storedKey + " != " + key + ")");
 			}
 
 			rv=o;
@@ -151,6 +142,20 @@ public class DiskCache extends Object {
 		return(rv);
 	}
 
+	public Set entrySet() {
+		Set rv=null;
+
+		try {
+			rv=new WalkerDiskCacheRanger();
+		} catch(ClassNotFoundException e) {
+			throw new NestedRuntimeException("Error getting set", e);
+		} catch(IOException e) {
+			throw new NestedRuntimeException("Error getting set", e);
+		}
+
+		return(rv);
+	}
+
     /** 
      * Old API for getting objects.
 	 *
@@ -158,6 +163,174 @@ public class DiskCache extends Object {
      */
     public Object getObject(String key) {
 		return(get(key));
+	}
+
+	private class WalkerDiskCacheRanger extends HashSet {
+
+		public WalkerDiskCacheRanger()
+			throws IOException, ClassNotFoundException {
+			super();
+
+			init(new File(basedir));
+		}
+
+		private void init(File f) throws IOException, ClassNotFoundException {
+			if(f.isDirectory()) {
+				// If it's a directory, recurse
+				File stuff[]=f.listFiles();
+				if(stuff!=null) {
+					for(int i=0; i<stuff.length; i++) {
+						init(stuff[i]);
+					}
+				}
+			} else {
+				// Regular file, open it and read it
+				FileInputStream istream = new FileInputStream(f);
+				ObjectInputStream p = new ObjectInputStream(istream);
+				Object key=p.readObject();
+				p.close();
+				istream.close();
+
+				// Add the new entry.
+				add(new E(key, f));
+			}
+		}
+
+		/** 
+		 * Get an iterator.
+		 */
+		public Iterator iterator() {
+			return(new I(this, super.iterator()));
+		}
+
+	}
+
+	// Iterator implementation
+	private class I extends Object implements Iterator {
+
+		private Set s=null;
+		private Iterator i=null;
+		private E current=null;
+		private boolean begun=false;
+
+		// Instatiate the iterator over the default iterator implementation
+		public I(Set s, Iterator i) {
+			super();
+			this.i=i;
+			this.s=s;
+		}
+
+		/** 
+		 * Get the next object.
+		 */
+		public Object next() {
+			begun=true;
+			current=(E)i.next();
+			return(current);
+		}
+
+		/** 
+		 * True if there's another entry.
+		 */
+		public boolean hasNext() {
+			return(i.hasNext());
+		}
+
+		/** 
+		 * Remove the given object.
+		 */
+		public void remove() {
+			if(begun==false) {
+				throw new IllegalStateException("Have not yet begun walking.");
+			}
+
+			if(current!=null) {
+				File f=current.getPath();
+				f.delete();
+			}
+			i.remove();
+		}
+
+	}
+
+	// Map entry implementation
+	private class E extends Object implements Map.Entry {
+
+		File path=null;
+		Object k=null;
+
+		public E(Object key, File path) {
+			super();
+			this.k=key;
+			this.path=path;
+		}
+
+		/** 
+		 * True if two objects are equal.
+		 */
+		public boolean equals(Object o) {
+			boolean rv=false;
+
+			if(o instanceof E) {
+				E e=(E)o;
+				rv=e.k.equals(k);
+			}
+
+			return(rv);
+		}
+
+		/** 
+		 * Get the path to the file.
+		 */
+		public File getPath() {
+			return(path);
+		}
+
+		/** 
+		 * Get the key.
+		 */
+		public Object getKey() {
+			return(k);
+		}
+
+		/** 
+		 * Get the hash code.
+		 */
+		public int hashCode() {
+			return(k.hashCode());
+		}
+
+		/** 
+		 * Get the value.
+		 */
+		public Object getValue() {
+			Object rv=null;
+
+			try {
+				FileInputStream istream = new FileInputStream(path);
+				ObjectInputStream p = new ObjectInputStream(istream);
+				Object key=p.readObject();
+				Object val=p.readObject();
+				p.close();
+				istream.close();
+
+				rv=val;
+			} catch(IOException e) {
+				throw new NestedRuntimeException("Error getting object",e);
+			} catch(ClassNotFoundException e) {
+				throw new NestedRuntimeException("Error getting object",e);
+			}
+
+			return(rv);
+		}
+
+		/** 
+		 * Not implemented.
+		 */
+		public Object setValue(Object o) {
+			throw new UnsupportedOperationException("Can't set here.");
+		}
+
 	}
 
 }
--- tests/net/spy/test/DiskCacheTest.java
+++ tests/net/spy/test/DiskCacheTest.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2002  Dustin Sallings
  *
- * $Id: DiskCacheTest.java,v 1.2 2002/09/13 06:52:23 dustin Exp $
+ * $Id: DiskCacheTest.java,v 1.3 2002/09/13 17:27:49 dustin Exp $
  */
 
 package net.spy.test;
@@ -9,7 +9,9 @@ package net.spy.test;
 import java.io.File;
 import java.io.IOException;
 
+import java.util.Set;
 import java.util.Map;
+import java.util.HashSet;
 import java.util.HashMap;
 import java.util.Iterator;
 
@@ -104,10 +106,7 @@ public class DiskCacheTest extends TestCase {
 		f.mkdirs();
 	}
 
-	/** 
-	 * Test basic functionality of the disk cache.
-	 */
-	public void testBasicDiskCache() throws Exception {
+	private Map initCache() throws IOException {
 		PwGen gen=new PwGen();
 		HashMap pairs=new HashMap();
 
@@ -117,6 +116,15 @@ public class DiskCacheTest extends TestCase {
 			pairs.put(key, value);
 			cache.storeObject(key, value);
 		}
+
+		return(pairs);
+	}
+
+	/** 
+	 * Test basic functionality of the disk cache.
+	 */
+	public void testBasicDiskCache() throws Exception {
+		Map pairs=initCache();
 
 		for(Iterator i=pairs.entrySet().iterator(); i.hasNext(); ) {
 			Map.Entry me=(Map.Entry)i.next();
@@ -129,15 +137,7 @@ public class DiskCacheTest extends TestCase {
 	 * Test basic functionality of the disk cache.
 	 */
 	public void testBasicDiskCacheNew() throws Exception {
-		PwGen gen=new PwGen();
-		HashMap pairs=new HashMap();
-
-		for(int i=0; i<1000; i++) {
-			String key=gen.getPass(8);
-			String value=gen.getPass(8);
-			pairs.put(key, value);
-			cache.put(key, value);
-		}
+		Map pairs=initCache();
 
 		for(Iterator i=pairs.entrySet().iterator(); i.hasNext(); ) {
 			Map.Entry me=(Map.Entry)i.next();
@@ -145,5 +145,73 @@ public class DiskCacheTest extends TestCase {
 			assertEquals(me.getValue(), cache.get(me.getKey()));
 		}
 	}
+
+	/** 
+	 * Test cache walking.
+	 */
+	public void testCacheWalking() throws Exception {
+		Map pairs=initCache();
+
+		// First, walk the map
+		for(Iterator i=pairs.entrySet().iterator(); i.hasNext(); ) {
+			Map.Entry me=(Map.Entry)i.next();
+
+			assertEquals(me.getValue(), cache.get(me.getKey()));
+		}
+
+		// Now, walk the cache
+		int n=0;
+		Set s=new HashSet();
+		for(Iterator i=cache.entrySet().iterator(); i.hasNext(); ) {
+			Map.Entry me=(Map.Entry)i.next();
+
+			// Make sure we haven't seen this record before
+			assertTrue("Already seen " + me.getKey() + " at " + n,
+				(!s.contains(me.getKey())));
+			s.add(me.getKey());
+
+			assertEquals(me.getValue(), pairs.get(me.getKey()));
+
+			// Make we don't run over
+			assertTrue("Too many entries!", n<pairs.size());
+			n++;
+		}
+
+		assertEquals("pairs.size() != cache.size()",
+			pairs.size(), cache.size());
+	}
+
+	/** 
+	 * Test cache walking.
+	 */
+	public void testCacheWalkingWithRemoval() throws Exception {
+		Map pairs=initCache();
+
+		// Now, walk the cache, removing every other entry
+		Set cacheSet=cache.entrySet();
+		int origsize=cache.size();
+		int newsize=origsize;
+		int n=0;
+		for(Iterator i=cacheSet.iterator(); i.hasNext(); ) {
+			Map.Entry me=(Map.Entry)i.next();
+
+			// Remove every other entry.
+			if(n%2 == 0) {
+				i.remove();
+				newsize--;
+			}
+
+			// Make we don't run over
+			assertTrue("Removing more entries than we started with", newsize>0);
+			n++;
+		}
+
+		assertTrue("Sizes didn't change", newsize<origsize);
+		assertEquals("Incorrect new size in Set", newsize, cacheSet.size());
+		cacheSet=cache.entrySet();
+		assertEquals("Incorrect new size in Cache", newsize, cacheSet.size());
+
+	}
+
 
 }
