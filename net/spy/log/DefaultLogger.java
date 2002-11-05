538a7b6fcdd936b3fe44ef8599cdbb0d26d5b9d2
Dustin Sallings <dustin@spy.net>
2002-11-05 15:36:19 +0000

    Move this back up to > INFO since I've got proper logge... Keywords: cscvs:MAIN-86
    Author: dustin
    Date: 2002/11/05 07:36:19
    
    Move this back up to > INFO since I've got proper logger support.
    
    imported from: dustin@spy.net--java/spyjar--sfcvs--1.0--patch-86

--- net/spy/log/DefaultLogger.java
+++ net/spy/log/DefaultLogger.java
@@ -1,6 +1,6 @@
 // Copyright (c) 2002  SPY internetworking <dustin@spy.net>
 //
-// $Id: DefaultLogger.java,v 1.2 2002/11/04 21:16:09 dustin Exp $
+// $Id: DefaultLogger.java,v 1.3 2002/11/05 07:36:19 dustin Exp $
 
 package net.spy.log;
 
@@ -8,7 +8,7 @@ package net.spy.log;
  * Default logger implementation.
  *
  * This logger is really primitive.  It just logs everything to stderr if
- * it's higher than debug.
+ * it's higher than INFO.
  */
 public class DefaultLogger extends AbstractLogger {
 
@@ -23,7 +23,7 @@ public class DefaultLogger extends AbstractLogger {
 	 * @see AbstractLogger
 	 */
 	protected void logAt(int level, Object message, Throwable e) {
-		if(level >= INFO) {
+		if(level > INFO) {
 			System.err.println(getName() + " (" + levelToString(level)
 				+ "): " + message);
 			if(e != null) {
