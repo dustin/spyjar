d583f210b1114b111b7326d6e8ed094cfb90f678
Dustin Sallings <dustin@spy.net>
2002-11-05 05:16:09 +0000

    I changed what it did, so I figured I'd better have the... Keywords: cscvs:MAIN-81
    Author: dustin
    Date: 2002/11/04 21:16:09
    
    I changed what it did, so I figured I'd better have the docs agree.
    
    imported from: dustin@spy.net--java/spyjar--sfcvs--1.0--patch-81

--- net/spy/log/DefaultLogger.java
+++ net/spy/log/DefaultLogger.java
@@ -1,14 +1,14 @@
 // Copyright (c) 2002  SPY internetworking <dustin@spy.net>
 //
-// $Id: DefaultLogger.java,v 1.1 2002/11/04 21:13:57 dustin Exp $
+// $Id: DefaultLogger.java,v 1.2 2002/11/04 21:16:09 dustin Exp $
 
 package net.spy.log;
 
 /**
  * Default logger implementation.
  *
- * This logger is really primitive.  It just sinks everything below error.
- * Error and fatal are send to stderr.
+ * This logger is really primitive.  It just logs everything to stderr if
+ * it's higher than debug.
  */
 public class DefaultLogger extends AbstractLogger {
 
