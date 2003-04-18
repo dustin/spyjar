2ed1b25de0bf2a1c2b7294b49ecc589800ccdd72
Dustin Sallings <dustin@spy.net>
2003-04-18 16:28:53 +0000

    Send the notify from the update since I no longer send ... Keywords: cscvs:MAIN-212
    Author: dustin
    Date: 2003/04/18 08:28:53
    
    Send the notify from the update since I no longer send it from Cron.
    
    imported from: dustin@spy.net--java/spyjar--sfcvs--1.0--patch-212

--- net/spy/net/URLItem.java
+++ net/spy/net/URLItem.java
@@ -1,6 +1,6 @@
 // Copyright (c) 2001  Dustin Sallings <dustin@spy.net>
 //
-// $Id: URLItem.java,v 1.2 2003/04/14 04:00:28 dustin Exp $
+// $Id: URLItem.java,v 1.3 2003/04/18 08:28:54 dustin Exp $
 
 package net.spy.net;
 
@@ -81,6 +81,11 @@ public class URLItem extends Job {
 			content=hf.getData();
 		} catch(IOException e) {
 			lastError=e;
+		}
+
+		// Notify listeners that this has been updated.
+		synchronized(this) {
+			notify();
 		}
 	}
 
