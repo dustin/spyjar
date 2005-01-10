6d181f94bf9246fba4933a1fe9f0aceb4934e6c9
Dustin Sallings <dustin@spy.net>
2005-01-10 16:47:55 +0000

    Added the Collection saver.
    imported from: dustin@spy.net--java/spyjar--head--2.0--patch-43

---
+++ src/net/spy/db/savables/CollectionSavable.java
@@ -0,0 +1,45 @@
+// Copyright (c) 2005  Dustin Sallings <dustin@spy.net>
+// arch-tag: 0E0FDEBC-62E4-11D9-AFF7-000A957659CC
+
+package net.spy.db.savables;
+
+import java.util.Collection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import net.spy.db.Savable;
+import net.spy.db.SaveContext;
+import net.spy.db.SaveException;
+import net.spy.db.AbstractSavable;
+
+/**
+ * Wrapper to save an existing collection.
+ */
+public class CollectionSavable extends AbstractSavable {
+
+	private Collection collection=null;
+
+	/**
+	 * Get an instance of CollectionSavable.
+	 */
+	public CollectionSavable(Collection c) {
+		super();
+		this.collection=c;
+	}
+
+	/** 
+	 * Get the collection.
+	 */
+	public Collection getPostSavables(SaveContext context) {
+		return(collection);
+	}
+
+	/** 
+	 * NOOP.
+	 */
+	public void save(Connection conn, SaveContext context)
+		throws SaveException, SQLException {
+	}
+
+}
